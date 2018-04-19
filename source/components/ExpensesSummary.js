779164159e0b89d8330b316019a5d1bfc584c38e
Daniel Langenakker <dlangenakker@gmail.com>
2018-04-19 10:42:38 +1000

    Changed code to better reflect course while keeping original code in comments

--- source/components/ExpensesSummary.js
+++ source/components/ExpensesSummary.js
@@ -4,7 +4,25 @@ import selectExpensesTotal from '../selectors/expenses-total.js';
 import selectExpenses from '../selectors/expenses';
 import numeral from 'numeral';
 
-export const ExpensesSummary = (props) => (
+export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
+	const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+	const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00');
+	return (
+		<div>
+			<p>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</p>
+		</div>
+	);
+};
+
+const mapStateToProps = (state) => {
+	const visibleExpenses = selectExpenses(state.expenses, state.filters);
+	return {
+		expenseCount: visibleExpenses.length, 
+		expensesTotal: selectExpensesTotal(visibleExpenses)
+	};
+};
+
+/*export const ExpensesSummary = (props) => (
 	<div>
 		<p>Viewing {props.expenses.length} {props.expenses.length === 1 ? 'expense' : 'expenses'} totalling {numeral(selectExpensesTotal(props.expenses) / 100).format('$0,0.00')}</p>
 	</div>
@@ -14,6 +32,6 @@ const mapStateToProps = (state) => {
 	return {
 		expenses: selectExpenses(state.expenses, state.filters)
 	};
-};
+};*/
 
 export default connect(mapStateToProps)(ExpensesSummary);
--- source/tests/components/ExpensesSummary.test.js
+++ source/tests/components/ExpensesSummary.test.js
@@ -4,11 +4,11 @@ import {shallow} from 'enzyme';
 import expenses from '../fixtures/expenses';
 
 test('Should render ExpensesSummary correctly with many expenses', () => {
-	const wrapper = shallow(<ExpensesSummary  expenses={expenses} />);
+	const wrapper = shallow(<ExpensesSummary  expenseCount={3} expensesTotal={12345} />);
 	expect(wrapper).toMatchSnapshot();
 });
 
 test('Should render ExpensesSummary correctly with 1 expense', () => {
-	const wrapper = shallow(<ExpensesSummary  expenses={[expenses[0]]} />);
+	const wrapper = shallow(<ExpensesSummary  expenseCount={1} expensesTotal={1234} />);
 	expect(wrapper).toMatchSnapshot();
 });
